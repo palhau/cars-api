1ed6b754d58ddadef745c007be76de54fdd2b71e
Alan Palhau <alanvieirapalhau@hotmail.com>
2023-07-31 16:19:09 -0300

    fix: added node-fetch to make the call simplier

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,101 +1,67 @@
 const CarLog = require('../models/index.js');
 const { randomUUID } = require('crypto');
-const http = require('http');
-
 const { createdCarsQueue } = require('../queues/createdCars.js');
 
-module.exports.create = (req, res) => {
-  let data = '';
+module.exports.create = async (req, res) => {
+  try {
+    const fetch = (await import('node-fetch')).default;
+    if (!Object.keys(req.body).length) {
+      return res.status(400).json({
+        message: 'Form content can not be empty',
+      });
+    }
 
-  // Validade request
-  if (!Object.keys(req.body).length) {
-    return res.status(400).json({
-      message: 'Form content can not be empty',
+    const postData = JSON.stringify({
+      title: req.body.title,
+      brand: req.body.brand,
+      price: req.body.price,
+      age: req.body.age,
+    });
+
+    const response = await fetch('http://api-test.bhut.com.br:3000/api/cars', {
+      method: 'POST',
+      body: postData,
+      headers: {'Content-Type': 'application/json'}
+    });
+    const data = await response.json();
+
+    const carLog = new CarLog({
+      car_id: data._id,
+      data_hora: Date.now(),
+    });
+
+    createdCarsQueue.add({ ...postData, id: data._id });
+
+    return carLog
+      .save()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        res.status(500).send({
+          message:
+            error.message + data._id || 'Some error ocurred while creating the car log.',
+        });
+      });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || 'Some error ocurred while creating the car.',
     });
   }
-
-  const postData = JSON.stringify({
-    _id: req.body._id,
-    title: req.body.title,
-    brand: req.body.brand,
-    price: req.body.price,
-    age: req.body.age,
-  });
-
-  const CreateCarOptions = {
-    hostname: 'http://api-test.bhut.com.br:3000/api',
-    path: '/cars',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(postData),
-    },
-  };
-
-  const request = http.request(CreateCarOptions, (response) => {
-    response.setEncoding('utf8');
-    response.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    response.on('end', () => {
-      const carLog = new CarLog({
-        id: randomUUID,
-        car_id: data._id,
-        data_hora: Date.now,
-      });
-
-      createdCarsQueue.add({ ...postData });
-
-      return carLog
-        .save()
-        .then((data) => {
-          res.json(data);
-        })
-        .catch((err) => {
-          res.status(500).send({
-            message:
-              err.message || 'Some error ocurred while creating the driver.',
-          });
-        });
-    });
-  });
-
-  request.on('error', (error) => {
-    console.error(error);
-  });
-
-  request.end();
 };
 
-module.exports.list = (_req, res) => {
-  let data = '';
-
-  const CreateCarOptions = {
-    hostname: 'http://api-test.bhut.com.br:3000/api',
-    path: '/cars',
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  const request = http.request(CreateCarOptions, (response) => {
-    response.setEncoding('utf8');
-    response.on('data', (chunk) => {
-      data += chunk;
+module.exports.list = async (_req, res) => {
+  try {
+    const fetch = (await import('node-fetch')).default;
+    const response = await fetch('http://api-test.bhut.com.br:3000/api/cars');
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).send({
+      message:
+        error.message || 'Some error ocurred while getting the car list.',
     });
-
-    response.on('end', () => {
-      res.json(data);
-    });
-  });
-
-  request.on('error', (error) => {
-    console.error(error);
-  });
-
-  request.end();
+  }
 };
 
 module.exports.logs = (_req, res) => {
@@ -106,14 +72,22 @@ module.exports.logs = (_req, res) => {
     .catch((err) => {
       res.status(500).json({
         message:
-          err.message || 'Some error ocurred while retrieving the drivers.',
+          err.message || 'Some error ocurred while retrieving the logs.',
       });
     });
 };
 
 module.exports.queueHook = (_req, res) => {
-  res.json({
-    message:
-      'Car Created Successfully',
-  });
+  try {
+    res.json({
+      message: 'Car Created Successfully',
+      id: req.body.id
+    });
+    res.status(200).end()
+  } catch (error) {
+    res.status(500).json({
+      message:
+      error.message || 'Some error ocurred while sending the message.',
+    });
+  }
 };
